f7f8bd638fa1963f4f2ea4233f63a8ef53986451
Marcos Davi <adamsdavi77@gmail.com>
2026-01-02 19:21:19 -0300

    Category add

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,19 @@ import { QuizContext } from "./context/quiz";
 import Welcome from "./components/Welcome";
 import Question from "./components/Question";
 import GameOver from "./components/GameOver";
+import PickCategory from "./components/PickCategory";
 
 import "./App.css";
 
 function App() {
   const [quizState, dispatch] = useContext(QuizContext);
 
-  useEffect(() => {
-    // embaralhar as perguntas
-    dispatch({ type: "REORDER_QUESTIONS" });
-  }, []);
-
   return (
     <>
       <div className="App">
         <h1>Quiz de Programação</h1>
         {quizState.gameStage === "Start" && <Welcome />}
+        {quizState.gameStage === "Category" && <PickCategory />}
         {quizState.gameStage === "Playing" && <Question />}
         {quizState.gameStage === "End" && <GameOver />}
       </div>
